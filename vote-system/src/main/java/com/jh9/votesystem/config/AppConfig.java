a5e08e22679ecff9a7c485397b61d44044e27d30
JH9 <jahyunnine@gmail.com>
2023-05-20 15:25:05 +0900

    refactor: config 패키지 이동

--- vote-system/src/main/java/com/jh9/votesystem/config/AppConfig.java
+++ vote-system/src/main/java/com/jh9/votesystem/config/AppConfig.java
@@ -1,4 +1,4 @@
-package com.jh9.votesystem.utils;
+package com.jh9.votesystem.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
