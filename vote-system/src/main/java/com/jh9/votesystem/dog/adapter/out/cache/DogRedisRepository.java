6a37f9349135a232b58ff9665da92b8b620532f4
JH9 <jahyunnine@gmail.com>
2023-05-23 22:33:46 +0900

    refactor: vote-system 캐싱 삭제 - 더 이상 필요하지 않은 캐시 의존성 및 기능들을 제거합니다.

--- vote-system/src/main/java/com/jh9/votesystem/dog/adapter/out/cache/DogRedisRepository.java
+++
@@ -1,7 +0,0 @@
-package com.jh9.votesystem.dog.adapter.out.cache;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface DogRedisRepository extends CrudRepository<DogRedisEntity, Long> {
-
-}
--- vote-system/src/main/java/com/jh9/votesystem/dog/application/DogService.java
+++ vote-system/src/main/java/com/jh9/votesystem/dog/application/DogService.java
@@ -2,8 +2,6 @@ package com.jh9.votesystem.dog.application;
 
 import com.jh9.votesystem.dog.application.port.in.DogSearchCondition;
 import com.jh9.votesystem.dog.application.port.in.DogUseCase;
-import com.jh9.votesystem.dog.application.port.out.cache.CachePort;
-import com.jh9.votesystem.dog.application.port.out.persistence.DogCommandPort;
 import com.jh9.votesystem.dog.application.port.out.persistence.DogQueryPort;
 import com.jh9.votesystem.dog.domain.Dog;
 import java.util.List;
@@ -13,37 +11,21 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class DogService implements DogUseCase {
 
-    private final CachePort cachePort;
-    private final DogCommandPort dogCommandPort;
     private final DogQueryPort dogQueryPort;
 
-    public DogService(CachePort cachePort, DogCommandPort dogCommandPort, DogQueryPort dogQueryPort) {
-        this.cachePort = cachePort;
-        this.dogCommandPort = dogCommandPort;
+    public DogService(DogQueryPort dogQueryPort) {
         this.dogQueryPort = dogQueryPort;
     }
 
     @Override
-    @Transactional
-    public void createCandidate(Dog dog) {
-        dogCommandPort.save(dog);
-    }
-
-    @Override
     @Transactional(readOnly = true)
-    public List<Dog> findByCondition(DogSearchCondition condition) {
-        return dogQueryPort.findAll(condition);
+    public List<Long> findByCondition(DogSearchCondition condition) {
+        return dogQueryPort.findByCondition(condition);
     }
 
     @Override
     @Transactional(readOnly = true)
     public Dog findById(Long id) {
-        try {
-            return cachePort.get(id);
-        } catch (IllegalArgumentException e) {
-            Dog queryedDog = dogQueryPort.findById(id);
-            cachePort.save(queryedDog);
-            return queryedDog;
-        }
+        return dogQueryPort.findById(id);
     }
 }
